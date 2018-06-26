c4d081ed82da2358127b710cde2cf462a867d10f
maniscab9940 <40374843+maniscab9940@users.noreply.github.com>
2018-06-25 22:42:47 -0400

    Add files via upload
    distance traveled

---
+++ P2HW1_DistanceTraveled_LundyBrittany.py
@@ -0,0 +1,16 @@
+#CTI-110
+#P2HW1 - Distance Traveled
+# Brittany Lundy
+#06/19/2018
+
+#Distance = Speed * Time
+
+#A car is traveling at 70 miles per hour. Display the following:
+#The distance the car will travel in 6 hours
+#The distance the car will travel in 10 hours
+#The distance the car will travel in 15 hours
+
+speed = 70
+print("In 6 hours, the car will travel " + str((speed * 6)) + "miles" )
+print("In 10 hours, the car will travel " + str((speed * 10)) + "miles" )
+print ("In 15 hours, the car will travel " + str((speed * 15)) + "miles" )
